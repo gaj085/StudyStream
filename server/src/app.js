385394fc6a0c750a2b007847533670b11fcffc21
gaj085 <gajendraverma085@gmail.com>
2026-09-10 18:50:03 +0530

    add root backend status route

--- server/src/app.js
+++ server/src/app.js
@@ -29,6 +29,10 @@ app.use(
 app.use(express.json());
 app.use(morgan("dev"));
 
+app.get("/", (req, res) => {
+  res.send("StudyStream backend is running!");
+});
+
 // Routes
 app.use("/api/videos", videoRoutes);
 app.use("/api", healthRoutes);
